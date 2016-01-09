c183df3ac4d790ac39df9e560f83a16c5c0e468d
Dimitar Danailov <dimitar.danailov@mentormate.com>
2016-01-09 17:25:43 +0200

    Add .gitignore for dynamic txt files and get original join1_mapper and join1_reducer.

--- JoiningData/join1_reducer.py
+++ JoiningData/join1_reducer.py
@@ -2,7 +2,7 @@
 import sys
 
 # --------------------------------------------------------------------------
-# This reducer code will input a <word, value> input file, and join words together
+#This reducer code will input a <word, value> input file, and join words together
 # Note the input will come as a group of lines with same word (ie the key)
 # As it reads words it will hold on to the value field
 #
@@ -13,69 +13,72 @@ import sys
 #   
 # At the end it will perform the last join
 #
-# Note, there is NO error checking of the input, it is assumed to be correct, meaning
-#  it has word with correct and matching entries, no extra spaces, etc.
 #
-# See https://docs.python.org/2/tutorial/index.html for python tutorials
+#  Note, there is NO error checking of the input, it is assumed to be correct, meaning
+#   it has word with correct and matching entries, no extra spaces, etc.
 #
-# San Diego Supercomputer Center copyright
+#  see https://docs.python.org/2/tutorial/index.html for python tutorials
+#
+#  San Diego Supercomputer Center copyright
 # --------------------------------------------------------------------------
 
-# initialize previous word  to blank string
-prev_word = "  "                
+prev_word          = "  "                #initialize previous word  to blank string
+months             = ['Jan','Feb','Mar','Apr','Jun','Jul','Aug','Sep','Nov','Dec']
 
-months = ['Jan','Feb','Mar','Apr','Jun','Jul','Aug','Sep','Nov','Dec']
-
-dates_to_output = [] # an empty list to hold dates for a given word
-day_cnts_to_output = [] # an empty list of day counts for a given word
+dates_to_output    = [] #an empty list to hold dates for a given word
+day_cnts_to_output = [] #an empty list of day counts for a given word
 # see https://docs.python.org/2/tutorial/datastructures.html for list details
 
-# count input lines
-line_cnt = 0
+line_cnt           = 0  #count input lines
 
 for line in sys.stdin:
-	line = line.strip() # string out carriage return
-	key_value = line.split('\t') # split line, into key and value, returns a list
-	line_cnt = line_cnt + 1
+    line       = line.strip()       #strip out carriage return
+    key_value  = line.split('\t')   #split line, into key and value, returns a list
+    line_cnt   = line_cnt+1     
 
-	# note: for simple debuggin use print statements, ie:
-	curr_word = key_value[0] # key is first item in list, indexed by 0
-	value_in = key_value[1] # value is 2nd item
+    #note: for simple debugging use print statements, ie:  
+    curr_word  = key_value[0]         #key is first item in list, indexed by 0
+    value_in   = key_value[1]         #value is 2nd item
 
-	#-----------------------------------------------------
-	# Check if its a new word and not the first line 
-	#   (b/c for the first line the previous word is not applicable)
-	#   if so then print out list of dates and counts
-	#----------------------------------------------------
-	if curr_word != prev_word:
-		# -----------------------
-		# now write out the join result, but not for the first line input
-		# -----------------------
-		if line_cnt > 1:
-			for i in range(len(dates_to_output)): # loop thru dates, indexes start at 0
-				print('{0} {1} {2} {3}'.format(dates_to_output[i],prev_word,day_cnts_to_output[i],curr_word_total_cnt))
-			# now reset list
-			dates_to_output = []
-			day_cnts_to_output = []
-		prev_word = curr_word # set up previous word for the next set of input files
+    #-----------------------------------------------------
+    # Check if its a new word and not the first line 
+    #   (b/c for the first line the previous word is not applicable)
+    #   if so then print out list of dates and counts
+    #----------------------------------------------------
+    if curr_word != prev_word:
+
+        # -----------------------     
+	#now write out the join result, but not for the first line input
+        # -----------------------
+        if line_cnt>1:
+	    for i in range(len(dates_to_output)):  #loop thru dates, indexes start at 0
+	         print('{0} {1} {2} {3}'.format(dates_to_output[i],prev_word,day_cnts_to_output[i],curr_word_total_cnt))
+            #now reset lists
+	    dates_to_output   =[]
+            day_cnts_to_output=[]
+        prev_word         =curr_word  #set up previous word for the next set of input lines
+
 	
-	# ---------------------------------------------------------------
-	# whether or not the join result was written out, now process the curr word    
-	# determine if its from file <word, total-count> or < word, date day-count>
-	# and build up list of dates, day counts, and the 1 total count
-	# ---------------------------------------------------------------
-	if (value_in[0:3] in months):
-		date_day = value_in.split() # split the value field into a date and day_cnt
+    # ---------------------------------------------------------------
+    #whether or not the join result was written out, 
+    #   now process the curr word    
+  	
+    #determine if its from file <word, total-count> or < word, date day-count>
+    # and build up list of dates, day counts, and the 1 total count
+    # ---------------------------------------------------------------
+    if (value_in[0:3] in months): 
 
-		# Addd date to list of the fields we are building
-		dates_to_output.append(date_day[0])
-		day_cnts_to_output.append(date_day[0])
-		day_cnts_to_output.append(date_day[1])
-	else:
-		curr_word_total_cnt = value_in # if the value field was just the total count the its 
-																	 # the first (and only) item in this list
+        date_day =value_in.split() #split the value field into a date and day-cnt
+        
+        #add date to lists of the value fields we are building
+        dates_to_output.append(date_day[0])
+        day_cnts_to_output.append(date_day[1])
+    else:
+        curr_word_total_cnt = value_in  #if the value field was just the total count then its
+                                           #the first (and only) item in this list
+
 # ---------------------------------------------------------------
 #now write out the LAST join result
 # ---------------------------------------------------------------
 for i in range(len(dates_to_output)):  #loop thru dates, indexes start at 0
-	print('{0} {1} {2} {3}'.format(dates_to_output[i],prev_word,day_cnts_to_output[i],curr_word_total_cnt))
+         print('{0} {1} {2} {3}'.format(dates_to_output[i],prev_word,day_cnts_to_output[i],curr_word_total_cnt))
